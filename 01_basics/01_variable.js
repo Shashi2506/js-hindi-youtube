db399b0e3d40b43017e309e8f3f9149c01ebe26e
Shashi Ranjan Kumar <121653360+Shashi2506@users.noreply.github.com>
2024-03-25 10:31:01 +0000

    datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId=1234
+let accountEmail="shashiranjanky9523@gmail.com"
+var accountPassword=9876
+accountCity="patna"
+
+// accountId=2
+accountEmail="hc@gmail.com"
+accountPassword=99999
+accountCity="darbhanga"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,15 @@
+// data type
+//number
+//string 
+//boolean
+//null
+//undefine
+//symbol
+
+let name="shashi"
+let age=20
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
