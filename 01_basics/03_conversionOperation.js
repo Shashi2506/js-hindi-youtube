184c32927e78bd3ad12cff18c39c19f8b82aa316
Shashi Ranjan Kumar <121653360+Shashi2506@users.noreply.github.com>
2024-03-26 14:50:38 +0000

    conversion to datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -34,3 +34,27 @@ let someNumber=33
 let stringNumber=string(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// ************************  operation ******************************
+
+let value=3;
+let negValue=-value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%2);
+
+let str1="hello"
+let str2=" shashi"
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
