52674ad117f9fda9aa1e1c29dea7bfbca483d2e0
Shashi Ranjan Kumar <121653360+Shashi2506@users.noreply.github.com>
2024-03-26 13:49:10 +0000

    conversion to datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score=33
+console.log(typeof(score));
+
+let score2="33"
+console.log(typeof(score2));
+
+let score3="33"
+let valueInNumber=number(score3)
+console.log(typeof(valueInNumber));
+
+let score4="33abc"
+let valueInNumber1=number(score4)
+console.log(typeof(valueInNumber1));
+console.log(valueInNumber1);
+
+
+// let isLoggedIn=1
+// let booleanIsLogged=Boolean(isLoggedIn)
+// console.log(booleanIsLogged);
+
+
+// let isLoggedIn=""
+// let booleanIsLogged=Boolean(isLoggedIn)
+// console.log(booleanIsLogged);
+
+
+
+let isLoggedIn="shashi"
+let booleanIsLogged=Boolean(isLoggedIn)
+console.log(booleanIsLogged);
+
+
+let someNumber=33
+let stringNumber=string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
