596795f4ea3d0758f291770a0ccdba6a7662b32b
Shashi Ranjan Kumar <121653360+Shashi2506@users.noreply.github.com>
2024-03-27 13:41:25 +0000

    number and math

---
+++ 01_basics/07_nums_math.js
@@ -0,0 +1,38 @@
+// const score=400
+// console.log(score);
+
+// const balance=new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+// const otherNumber=123.8966
+// console.log(otherNumber.toPrecision(4));
+
+
+// const hundres=1000000
+// console.log(hundres.toLocaleString('en-In'));
+
+
+// *************************  Maths  ******************************
+
+
+// console.log(Math);
+
+// console.log(math.abs(4));
+// console.log(math.round(4.6));
+// console.log(ceil(4.2));
+// console.log(math.floor(4.9));
+
+// console.log(math.min(4,5,2,6,9));
+// console.log(Math.max(4,3,6,9));
+
+
+console.log(Math.random());
+
+console.log(Math.random()*10+1);
+
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random() *(max-min)) +min);
