2e4e051981a9d2e917d87f8f8439b018ba506094
Shashi Ranjan Kumar <121653360+Shashi2506@users.noreply.github.com>
2024-03-27 04:58:13 +0000

    datatype-summary

---
+++ 01_basics/05_datatype-summary.js
@@ -0,0 +1,30 @@
+
+// primitive datatype
+
+// string,number,boolean,null,undefine,symbol,BigInt
+
+// Reference (Non-Primitive)
+
+// Array , objects , Function
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId);
+
+
+// const bigNumber=34563456768761n
+
+
+// Array
+const heros=["shashi","ranjan","Yadav"];
+
+// object 
+let myObj={
+    name:"shashi",
+    age:22
+}
+
+// function 
+const myFunction=function(){
+    console.log("hello world");
+}
